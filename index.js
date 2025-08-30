5b8a9b75ee6b213a579b0d64af7f3b6d8ef4e0c9
freeandopentrpg-boop <freeandopentrpg@gmail.com>
2025-08-30 21:30:14 +0800

    更新了示範用團錄

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ var INDEX = [
 	{
 		system: "某系統",
 		title: "某團務的標題名稱",
-		url: ".template",
-		imgUrl: "src/img/replay-cover/example.jpg",
-		info: {
-			system: "某個系統",
-			gm: "...",
-			member: "A、B、C、D",
-			startDate: "0000/00/00",
-		}
+		url: "示範用團錄",
 	},
-
+	{
+		system: "某系統",
+		title: "某團務的標題名稱",
+		url: "示範用團錄",
+	},{
+		system: "某系統",
+		title: "某團務的標題名稱",
+		url: "示範用團錄",
+	},{
+		system: "某系統",
+		title: "某團務的標題名稱",
+		url: "示範用團錄",
+	},
 ];
---
+++ 示範用團錄/index.js
@@ -0,0 +1,12 @@
+var TITLE = "示範用團錄";
+var INDEX = [
+	{ title: "群組 (第一層)", members: [
+		{	title: "序幕", url: "20231217-00.html" },
+		{	title: "第一章", url: "20231217-01.html" },
+		{	title: "範例頁面 3", url: "example.html" },
+		{	title: "範例頁面 4", url: "example.html" },
+
+	]},
+	{	title: "角色卡", url: "https://docs.google.com/spreadsheets/d/1cwHdkD2ZBFjRcfAlKOFNbWKg_Ohz-FNeIdJMIeiiRPw/edit?gid=0#gid=0" },
+	
+];
